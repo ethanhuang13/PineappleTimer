3e44ea1c904eafb251c4fbeabbfb9e74b74a6d4f
Ethan Huang <blesserx@gmail.com>
2019-09-11 16:06:24 +0800

    Initial Commit

---
+++ PineappleTimer WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PineappleTimer WatchKit Extension
+//
+//  Created by Ethanhuang on 9/11/19.
+//  Copyright © 2019 Elaborapp Co., Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PineappleTimer WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  PineappleTimer WatchKit Extension
+//
+//  Created by Ethanhuang on 9/11/19.
+//  Copyright © 2019 Elaborapp Co., Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
