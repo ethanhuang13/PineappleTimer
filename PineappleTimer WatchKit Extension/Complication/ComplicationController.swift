f87fda9069e365aa957578bcf082d9172d8a9801
Ethan Huang <blesserx@gmail.com>
2019-09-15 13:42:55 +0800

    Add test complication

--- PineappleTimer WatchKit Extension/Complication/ComplicationController.swift
+++ PineappleTimer WatchKit Extension/Complication/ComplicationController.swift
@@ -8,20 +8,25 @@
 
 import ClockKit
 
+let startDate = Date().addingTimeInterval(-15 * 60)
+let endDate = Date().addingTimeInterval(10 * 60)
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
+//        handler([.forward, .backward])
+        handler([])
     }
     
     func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
+//        handler(stardDate)
         handler(nil)
     }
     
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
+//        handler(endDate)
         handler(nil)
     }
     
@@ -33,7 +38,13 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
+
+        if let template = self.currentTemplate(family: complication.family) {
+            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(entry)
+        } else {
+            handler(nil)
+        }
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -50,7 +61,146 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+
+        let template = self.placeholderTemplate(family: complication.family)
+        handler(template)
     }
-    
+
+    func placeholderTemplate(family: CLKComplicationFamily) -> CLKComplicationTemplate? {
+        let appNameTextProvider = CLKSimpleTextProvider(text: "🍍計時器")
+        let simpleTextProvider = CLKSimpleTextProvider(text: "🍍")
+        let gaugeProvider = CLKSimpleGaugeProvider(style: .fill, gaugeColor: .yellow, fillFraction: 1)
+
+        switch family {
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallRingText()
+            template.textProvider = simpleTextProvider
+            template.tintColor = .yellow
+            template.ringStyle = .closed
+            return template
+        case .modularLarge:
+            let template = CLKComplicationTemplateModularLargeTallBody()
+            template.headerTextProvider = appNameTextProvider
+            template.bodyTextProvider = simpleTextProvider
+            return template
+        case .utilitarianSmall:
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.textProvider = simpleTextProvider
+            return template
+        case .utilitarianSmallFlat:
+            let template = CLKComplicationTemplateUtilitarianSmallFlat()
+            template.textProvider = simpleTextProvider
+            return template
+        case .utilitarianLarge:
+            let template = CLKComplicationTemplateUtilitarianLargeFlat()
+            template.textProvider = simpleTextProvider
+            return template
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallRingText()
+            template.textProvider = simpleTextProvider
+            return template
+        case .extraLarge:
+            let template = CLKComplicationTemplateExtraLargeRingText()
+            template.textProvider = simpleTextProvider
+            return template
+        case .graphicCorner:
+            let template = CLKComplicationTemplateGraphicCornerGaugeText()
+            template.outerTextProvider = simpleTextProvider
+            template.leadingTextProvider = CLKSimpleTextProvider(text: "0")
+            template.trailingTextProvider = CLKSimpleTextProvider(text: "25")
+            template.gaugeProvider = gaugeProvider
+            return template
+        case .graphicBezel:
+            let template = CLKComplicationTemplateGraphicBezelCircularText()
+            let circularTemplate = self.currentTemplate(family: .graphicCircular)
+            template.circularTemplate = circularTemplate as! CLKComplicationTemplateGraphicCircular
+            template.textProvider = simpleTextProvider
+            return template
+        case .graphicCircular:
+            let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            template.centerTextProvider = simpleTextProvider
+            template.gaugeProvider = gaugeProvider
+            return template
+        case .graphicRectangular:
+            let template = CLKComplicationTemplateGraphicRectangularTextGauge()
+            template.headerTextProvider = appNameTextProvider
+            template.body1TextProvider = CLKRelativeDateTextProvider(date: endDate, style: .naturalFull, units: [.minute, .second])
+            template.gaugeProvider = gaugeProvider
+            return template
+        @unknown default:
+            return nil
+        }
+    }
+
+    func currentTemplate(family: CLKComplicationFamily) -> CLKComplicationTemplate? {
+        let appNameTextProvider = CLKSimpleTextProvider(text: "🍍計時器")
+        let relativeDateTextProvider = CLKRelativeDateTextProvider(date: endDate, style: .offsetShort, units: [.minute])
+        let longRelativeDateTextProvider = CLKRelativeDateTextProvider(date: endDate, style: .naturalFull, units: [.minute, .second])
+        let simpleTextProvider = CLKSimpleTextProvider(text: "🍍")
+        let gaugeProvider = CLKTimeIntervalGaugeProvider(style: .fill, gaugeColors: [.green, .yellow, .orange], gaugeColorLocations: [0, 0.2, 0.8], start: startDate, end: endDate)
+        let fillFraction = Float(endDate.timeIntervalSince(Date()) / endDate.timeIntervalSince(startDate))
+
+        switch family {
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallRingText()
+            template.textProvider = relativeDateTextProvider
+            template.fillFraction = fillFraction
+            template.tintColor = .yellow
+            template.ringStyle = .closed
+            return template
+        case .modularLarge:
+            let template = CLKComplicationTemplateModularLargeTallBody()
+            template.headerTextProvider = appNameTextProvider
+            template.bodyTextProvider = longRelativeDateTextProvider
+            return template
+        case .utilitarianSmall:
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.textProvider = relativeDateTextProvider
+            template.fillFraction = fillFraction
+            return template
+        case .utilitarianSmallFlat:
+            let template = CLKComplicationTemplateUtilitarianSmallFlat()
+            template.textProvider = relativeDateTextProvider
+            return template
+        case .utilitarianLarge:
+            let template = CLKComplicationTemplateUtilitarianLargeFlat()
+            template.textProvider = longRelativeDateTextProvider
+            return template
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallRingText()
+            template.textProvider = relativeDateTextProvider
+            template.fillFraction = fillFraction
+            return template
+        case .extraLarge:
+            let template = CLKComplicationTemplateExtraLargeRingText()
+            template.textProvider = relativeDateTextProvider
+            return template
+        case .graphicCorner:
+            let template = CLKComplicationTemplateGraphicCornerGaugeText()
+            template.outerTextProvider = simpleTextProvider
+            template.leadingTextProvider = CLKSimpleTextProvider(text: "0")
+            template.trailingTextProvider = CLKSimpleTextProvider(text: "25")
+            template.gaugeProvider = gaugeProvider
+            return template
+        case .graphicBezel:
+            let template = CLKComplicationTemplateGraphicBezelCircularText()
+            let circularTemplate = self.currentTemplate(family: .graphicCircular)
+            template.circularTemplate = circularTemplate as! CLKComplicationTemplateGraphicCircular
+            template.textProvider = longRelativeDateTextProvider
+            return template
+        case .graphicCircular:
+            let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            template.centerTextProvider = simpleTextProvider
+            template.gaugeProvider = gaugeProvider
+            return template
+        case .graphicRectangular:
+            let template = CLKComplicationTemplateGraphicRectangularTextGauge()
+            template.headerTextProvider = appNameTextProvider
+            template.body1TextProvider = CLKRelativeDateTextProvider(date: endDate, style: .naturalFull, units: [.minute, .second])
+            template.gaugeProvider = gaugeProvider
+            return template
+        @unknown default:
+            return nil
+        }
+    }
 }
